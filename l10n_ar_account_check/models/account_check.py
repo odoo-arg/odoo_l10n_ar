6f3140b1e67854619303ca0909755c0bb6d70361
Daniel Crocco <dcrocco@openpyme.com.ar>
2017-08-01 15:46:47 +0000

    Borrado logs

--- l10n_ar_account_check/models/account_check.py
+++ l10n_ar_account_check/models/account_check.py
@@ -18,8 +18,6 @@
 
 from openerp import models, fields, api
 from openerp.exceptions import ValidationError, UserError
-import logging
-_logger = logging.getLogger(__name__)
 
 
 class AccountAbstractCheck(models.AbstractModel):
