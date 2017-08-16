f77a081bf590c240c9d04550f19b43aea68a5ce7
Marco Klemenc <mklemenc@openpyme.com.ar>
2017-08-16 12:45:54 -0300

    Se empiezan a armar los tests

--- l10n_ar_perceptions_sifere/tests/test_perceptions_sifere.py
+++ l10n_ar_perceptions_sifere/tests/test_perceptions_sifere.py
@@ -22,12 +22,54 @@ from odoo.tests import common
 class TestPerceptionsSifere(common.TransactionCase):
     def setUp(self):
         super(TestPerceptionsSifere, self).setUp()
-        self.perception_sifere = self.env['perception.sifere'].create({
-            'name': 'SIFERE',
-            'date_from': datetime.now(),
-            'date_to': datetime.now(),
+        self.country = self.env['res.country'].create({
+            'name': "Pais",
+            'code': "ZZ",
+        })
+        self.tax = self.env['account.tax'].create({
+            'name': "SIFERE",
+            'amount': 1
+        })
+        self.perception = self.env['perception.perception'].create({
+            'name': "SIFERE",
+            'tax_id': self.tax.id,
+            'type': 'gross_income',
+            'jurisdiction': 'nacional'
+        })
+        self.partner = self.env['res.partner'].create({
+            'name': "Cliente",
+            'country_id': self.country.id,
+        })
+        self.account_type = self.env['account.account.type'].create({
+            'name': "Tipo de cuenta",
+        })
+        self.account = self.env['account.account'].create({
+            'code': "Codigo",
+            'name': "Cuenta",
+            'user_type_id': self.account_type.id,
+        })
+        self.journal = self.env['account.journal'].create({
+            'name': "Diario",
+            'type': 'sale',
+            'code': "DIA",
+        })
+        self.invoice = self.env['account.invoice'].create({
+            'partner_id': self.partner.id,
+            'account_id': self.account.id,
+            'journal_id': self.journal.id,
         })
 
-    # TODO
+    def test_perception(self):
+        self.env['account.invoice.perception'].create({
+            'name': "Linea Percepcion",
+            'invoice_id': self.invoice.id,
+            'perception_id': self.perception.id,
+            'amount': 400,
+            'jurisdiction': 'nacional',
+        })
+        assert len(self.invoice.perception_ids) == 1
+
+
+
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
